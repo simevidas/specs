27d7caa238246d21171f17f58001ab9b5a9e2ea4
Šime Vidas <sime.vidas@gmail.com>
2015-09-29 04:46:31 +0200

    Set up grunt-sass task

--- Gruntfile.js
+++ Gruntfile.js
@@ -9,18 +9,33 @@ module.exports = function (grunt) {
 				}
 			}
 		},
+        sass: {
+            default: {
+                options: {
+                    'outputStyle': 'expanded'
+                },
+                files: {
+                    'css/w3viewer.css': 'sass/w3viewer.sass'
+                }
+            }
+        },
 		watch: {
 			options: {
 				livereload: true
 			},
 			html: {
 				files: ['index.html']
-			}
+			},
+            sass: {
+                files: ['sass/*.sass'],
+                tasks: ['sass:default']
+            }
 		}
 	});
 
 	grunt.loadNpmTasks('grunt-contrib-connect');
-	grunt.loadNpmTasks('grunt-contrib-watch')
+	grunt.loadNpmTasks('grunt-contrib-watch');
+    grunt.loadNpmTasks('grunt-sass');
 
 	grunt.registerTask('server', ['connect', 'watch']);
 };
